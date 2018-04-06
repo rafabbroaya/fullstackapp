06c89807c25523eb3de7c5b6e85f631683acdf31
rafabbroaya@hotmail.com <!ChangePwd@3001>
2018-04-06 18:25:43 -0500

    Settings remote API

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {AppService} from '../app.service';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Constants} from '../constants';
 
 @Component({
   selector: 'app-home',
@@ -14,7 +15,7 @@ export class HomeComponent {
   constructor(private appService: AppService, private http: HttpClient) {
     http.get('api/token').subscribe(data => {
       const token = data['token'];
-      http.get('http://localhost:8181/api/items',
+      http.get(Constants.INVENTORY_API_ENDPOINT + '/items',
         {headers: new HttpHeaders().set('X-Auth-Token', token)}).subscribe((result: { 'id': '', 'content': '' }) => this.greeting = result);
     });
   }
