295594886087b05167a0d3ab5463e16fffc2b319
rafabbroaya@hotmail.com <!ChangePwd@3001>
2018-04-10 09:07:54 -0500

    test

--- frontend/src/app/constants.ts
+++ frontend/src/app/constants.ts
@@ -1,3 +1,3 @@
 export class Constants {
-  public static INVENTORY_API_ENDPOINT = 'http://consolaweb.ddns.net:8484/inventory/api';
+  public static INVENTORY_API_ENDPOINT = 'http://consolaweb.ddns.net:8585/inventory/api';
 }
